35a4dd34b98385c78e3d19845b7ec7fab4dfa6ea
Debarghaya Mitra <175645893+Debarghaya2006@users.noreply.github.com>
2025-02-10 12:10:56 +0530

    Added week 3 question 1 in c lab

---
+++ Semester 2/C Lab/src/Week 03 Question 01.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void fourByNStarRectangle(unsigned long long int lines)
+{
+    unsigned long long int i, j;
+    for(i=1; i<=lines; printf("\n"), i++)
+	   {
+        for(j=1; j<=4; printf("*"), j++);
+   	}
+}
+int main()
+{
+    unsigned long long int numberOfLine, i, j;
+    printf("Enter the number of lines you want to print the pattern: ");
+   	scanf("%llu", &numberOfLine);
+	   fourByNStarRectangle(numberOfLine);
+}
