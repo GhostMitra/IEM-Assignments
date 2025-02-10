6cb1d5b393ff1f4be08f1f9b34bd21ed97b97845
Debarghaya Mitra <175645893+Debarghaya2006@users.noreply.github.com>
2025-02-10 12:50:41 +0530

    added week 3 question 3 in c lab

---
+++ Semester 2/C Lab/src/Week 03 Question 03.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void increasingNormalStarTriangle(unsigned long long int lines)
+{
+    unsigned long long int i, j;
+    for(i=1; i<=lines; printf("\n"), i++)
+    {
+	       for(j=lines-i; j>0; printf(" "), j--);
+	       for(j=1; j<=i; printf("* "), j++);
+	   }
+}
+int main()
+{
+    unsigned long long int numberOfLine, i, j;
+    printf("Enter the number of lines you want to print the pattern: ");
+    scanf("%llu", &numberOfLine);
+	   increasingNormalStarTriangle(numberOfLine);
+	   return 0;
+}
