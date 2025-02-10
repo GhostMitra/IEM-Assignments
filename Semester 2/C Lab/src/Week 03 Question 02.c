7e0890cfd1d66d9129e769158519ab75bfc44bcb
Debarghaya Mitra <175645893+Debarghaya2006@users.noreply.github.com>
2025-02-10 12:16:40 +0530

    added week 3 question 2 in c lab

---
+++ Semester 2/C Lab/src/Week 03 Question 02.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void increasingLeftRightAngledStarTriangle(unsigned long long int lines)
+{
+    unsigned long long int i, j;
+    for(i=1; i<=lines; printf("\n"), i++)
+   	{
+	       for(j=1; j<=i; printf("*"), j++);
+   	}
+}
+int main()
+{
+   	unsigned long long int numberOfLine, i, j;
+   	printf("Enter the number of lines you want to print the pattern: ");
+   	scanf("%llu", &numberOfLine);
+   	increasingLeftRightAngledStarTriangle(numberOfLine);
+}
