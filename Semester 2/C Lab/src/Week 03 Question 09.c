97c74c267851096f7be8ef3937bcdd620b792407
Debarghaya Mitra <175645893+Debarghaya2006@users.noreply.github.com>
2025-02-23 18:13:21 +0530

    Update c lab with week 03 Question 09

---
+++ Semester 2/C Lab/src/Week 03 Question 09.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+unsigned long long int factorial(unsigned long long int num) 
+{
+    return (num == 0 || num == 1) ? 1 : num * factorial(num - 1);
+}
+
+unsigned long long int combination(unsigned long long int total, unsigned long long int selection) 
+{
+    return factorial(total) / (factorial(selection) * factorial(total - selection));
+}
+
+void centeredPascalsTriangle(unsigned long long int lines)
+{
+    unsigned long long int i, j;
+    for (i = 1; i <= lines; i++) 
+    {
+        for (j = 1; j <= lines - i; printf(" "), j++);
+        for (j = 1; j <= i; printf("%d ", combination(i - 1, j - 1)), j++);
+        printf("\n");
+    }
+}
+int main()
+{
+    unsigned long long int numberOfLine;
+    printf("Enter the number of lines you want to print the pattern: ");
+    scanf("%llu", &numberOfLine);
+    centeredPascalsTriangle(numberOfLine);
+    return 0;
+}
